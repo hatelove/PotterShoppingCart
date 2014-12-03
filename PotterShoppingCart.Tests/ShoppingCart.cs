3b619f2c341a316d867969e9e4394cd295df0843
91 <jay.hatelove@gmail.com>
2014-12-03 16:53:01 +0800

    把書的本數算出來後，依據本數算折扣，通過第二個測試案例

--- PotterShoppingCart.Tests/ShoppingCart.cs
+++ PotterShoppingCart.Tests/ShoppingCart.cs
@@ -7,9 +7,22 @@ namespace PotterShoppingCart.Tests
 {
     class ShoppingCart
     {
+        private const double unitBookPrice = 100;
         internal double CalculateFee(Dictionary<string, int> dictionary)
         {
-            return 100;
+            var bookCount = dictionary.Sum(x => x.Value);
+            if (bookCount == 1)
+            {
+                return unitBookPrice * bookCount;
+            }
+            else if (bookCount == 2)
+            {
+                return unitBookPrice * bookCount * 0.95;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
