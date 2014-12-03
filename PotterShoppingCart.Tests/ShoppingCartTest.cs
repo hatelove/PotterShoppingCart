633268be027b6bd9aa3b42880ef7b1bb78893872
91 <jay.hatelove@gmail.com>
2014-12-03 16:50:37 +0800

    第二個test case, 得到紅燈

--- PotterShoppingCart.Tests/ShoppingCartTest.cs
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -28,5 +28,15 @@ namespace MyTest
             var expected = 100;
             Assert.AreEqual(expected, this.fee);
         }
+
+        [TestMethod]
+        public void Test_第一集買了一本_第二集也買了一本_價格應為190()
+        {
+            this._books.Add("first", 1);
+            this._books.Add("second", 1);
+            this.fee = this._target.CalculateFee(this._books);
+            var expected = 190;
+            Assert.AreEqual(expected, this.fee);
+        }
     }
 }
