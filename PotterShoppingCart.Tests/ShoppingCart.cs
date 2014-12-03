9230db8fb4b00a8283ff71ab9ef6d5cb2fe3ad63
91 <jay.hatelove@gmail.com>
2014-12-03 17:06:59 +0800

    加入四本書的折扣為0.8

--- PotterShoppingCart.Tests/ShoppingCart.cs
+++ PotterShoppingCart.Tests/ShoppingCart.cs
@@ -17,7 +17,8 @@ namespace PotterShoppingCart.Tests
             {
                 {1,1},
                 {2,0.95},
-                {3,0.9}
+                {3,0.9},
+                {4,0.8}
             };
         }
 
