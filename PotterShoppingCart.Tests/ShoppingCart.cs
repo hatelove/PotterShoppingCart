3c0bb2ecc41b160e74ee1f37cd1086a63ccea541
91 <jay.hatelove@gmail.com>
2014-12-03 16:44:41 +0800

    first red light

---
+++ PotterShoppingCart.Tests/ShoppingCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart.Tests
+{
+    class ShoppingCart
+    {
+        internal double CalculateFee(Dictionary<string, int> dictionary)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PotterShoppingCart.Tests/ShoppingCartTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart.Tests;
+
+namespace MyTest
+{
+    [TestClass]
+    public class PotterShoppingCartTest
+    {
+        private ShoppingCart _target;
+        private Dictionary<string, int> _books;
+        private double fee;
+
+        [TestInitialize]
+        public void TestInitialized()
+        {
+            this._target = new ShoppingCart();
+            this._books = new Dictionary<string, int>();
+            this.fee = 0;
+        }
+
+        [TestMethod]
+        public void Test_第一集買了一本_其他都沒買_價格應為100元()
+        {
+            this._books.Add("first", 1);
+            this.fee = this._target.CalculateFee(this._books);
+            var expected = 100;
+            Assert.AreEqual(expected, this.fee);
+        }
+    }
+}
